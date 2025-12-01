54972c854b8346cfb42cf798c8d27204f05572e3
alfitosantosa <sanskun02pg@gmail.com>
2025-12-01 18:10:05 +0700

    create api bulk delete

--- app/hooks/Users/useBulkUsersData.ts
+++ app/hooks/Users/useBulkUsersData.ts
@@ -7,7 +7,23 @@ export const useBulkCreateUserData = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (data: any) => {
-      const res = await axios.post("/api/userdata/bulk", data);
+      const res = await axios.post("/api/userdata/bulk/create", data);
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+};
+
+export const useBulkDeleteUserData = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: any) => {
+      const res = await axios.post("/api/userdata/bulk/delete", data);
       return res.data;
     },
     onSuccess: () => {
