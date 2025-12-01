9f69596403005fe8f7d61b7cb739fb71b82fee4c
santosoo <144679229+alfitosantosa@users.noreply.github.com>
2025-12-01 11:16:05 +0700

    Fix syntax error in matcher path configuration

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/",
-    "/:path*,
+    "/:path*",
     "/dashboard/:path*",
     // "/api/:path*"
   ],
